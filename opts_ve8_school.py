260d5038082180ebbec57c35cb80065db746ab87
leessu <leejae7124@gmail.com>
2026-05-05 00:29:10 +0900

    school ve8

--- opts_ve8_school.py
+++ opts_ve8_school.py
@@ -28,7 +28,7 @@ def parse_opts():
                  type=str,
                  help='Local path of videos', ),
             dict(name="--annotation_path",
-                 default='/shared/home/affctiv/sumin/VAANet/tools/annotations/ve8/ve8_03.json',
+                 default='/shared/home/affctiv/sumin/VAANet/tools/annotations/ve8/ve8_02.json',
                  type=str,
                  help='Local path of annotation file'),
             dict(name="--result_path",
